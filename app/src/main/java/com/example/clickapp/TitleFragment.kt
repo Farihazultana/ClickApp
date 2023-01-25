544e6794bdfea5507aaaad751c3eb01afa57ed99
Fariha Sultana <farihazultana@gmail.com>
2023-01-25 21:14:26 +0600

    Initial commit

---
+++ app/src/main/java/com/example/clickapp/TitleFragment.kt
@@ -0,0 +1,31 @@
+package com.example.clickapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.clickapp.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+    private lateinit var binding: FragmentTitleBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+//        Set click listeners
+            binding.titleFragmentBtPlay.setOnClickListener {
+                findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+            }
+//        Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
